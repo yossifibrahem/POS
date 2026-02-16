1434596fdd7a3d23457e1619ab714cc6c00287a2
yossifibrahem <yossif.phelopater.ibrahem@gmail.com>
2026-02-16 21:25:24 +0200

    remove old migration sql files

--- supabase/migrations/20260214100000_add_sold_products_update_delete_policies.sql
+++
@@ -1,8 +0,0 @@
--- Allow admins to update and delete sold_products (for returns)
-CREATE POLICY "Admins can update sold products" ON public.sold_products
-  FOR UPDATE TO authenticated
-  USING (public.is_admin(auth.uid()));
-
-CREATE POLICY "Admins can delete sold products" ON public.sold_products
-  FOR DELETE TO authenticated
-  USING (public.is_admin(auth.uid()));
